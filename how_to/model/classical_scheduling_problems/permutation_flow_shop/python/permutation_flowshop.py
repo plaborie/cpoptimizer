b203f4376d2e867753527d35cf7321b6d22260ad
Philippe Laborie <64833735+plaborie@users.noreply.github.com>
2021-03-29 11:17:08 +0200

    Update permutation_flowshop.py

--- how_to/model/classical_scheduling_problems/permutation_flow_shop/python/permutation_flowshop.py
+++ how_to/model/classical_scheduling_problems/permutation_flow_shop/python/permutation_flowshop.py
@@ -19,17 +19,16 @@ model = CpoModel()
 x = [ [interval_var(size=d) for d in J[i]] for i in N ]
 s = [ sequence_var([x[i][k] for i in N]) for k in M]
 
-# Objective: minimize makespan
-model.add(minimize(max([end_of(x[i][m-1]) for i in N])))
-
-# Constraints: operations do not overlap on machines
-model.add([ no_overlap(s[k])  for k in M])
-
-# Constraints: same order of operations on all machines
-model.add([ same_sequence(s[0],s[k]) for k in range(1,m)])
-
-# Constraints: precedence between consecutive operations of a job
-model.add([ end_before_start(x[i][j-1], x[i][j])  for i in N for j in range(1,m)])
+model.add(
+ # Objective: minimize makespan
+ [ minimize(max([end_of(x[i][m-1]) for i in N])) ] +
+ # Constraints: operations do not overlap on machines
+ [ no_overlap(s[k])  for k in M] +
+ # Constraints: same order of operations on all machines
+ [ same_sequence(s[0],s[k]) for k in range(1,m)] +
+ # Constraints: precedence between consecutive operations of a job
+ [ end_before_start(x[i][j-1], x[i][j])  for i in N for j in range(1,m)]
+)
 
 # 3. SOLVING THE PROBLEM
 
